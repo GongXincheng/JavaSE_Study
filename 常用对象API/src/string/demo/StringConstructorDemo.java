a3a68e4f2f6b17ed512805f9d3bd2ee4d458c40c
GongXincheng <https://github.com/GongXincheng>
2017-07-19 19:03:06 +0800

    String的构造方法

---
+++ 常用对象API/src/string/demo/StringConstructorDemo.java
@@ -0,0 +1,26 @@
+package string.demo;
+
+/*	2017年7月19日19:02:59
+ * String的构造方法演示*/
+public class StringConstructorDemo {
+	public static void main(String[] args) {
+		stringConstructorDemo2();
+	}
+
+	private static void stringConstructorDemo2() {
+		char[] arr = {'w','a','p','q','x'};
+		String s = new String(arr);
+		String s1 = new String(arr, 1, 3);
+		System.out.println("s = " +s);	//s = wapqx
+		System.out.println("s1 = " +s1);	//s1 = apq
+	}
+
+	private static void stringConstructorDemo1() {
+		String s = new String();   //等效于 String s = "";
+		
+		byte[] arr = {65,66,67,68,69,70};
+		String s1 = new String(arr);
+		System.out.println("s1 = " +s1);	//s1 = ABCDEF
+	}
+
+}
