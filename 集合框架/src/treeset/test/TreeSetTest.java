d29d3b204c719f70dfd88959cf64073b16973951
GongXincheng <https://github.com/GongXincheng>
2017-07-22 22:32:56 +0800

    TreeSetTest

---
+++ 集合框架/src/treeset/test/TreeSetTest.java
@@ -0,0 +1,24 @@
+package treeset.test;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/*
+ * 	2017年7月22日 22:19:24
+ * 	对字符串的长度排序
+ */
+public class TreeSetTest {
+	public static void main(String[] args) {
+		TreeSet ts = new TreeSet( new ComparatorByLength());
+		ts.add("aaaaa");
+		ts.add("zz");
+		ts.add("nbaq");
+		ts.add("cba");
+		ts.add("abcbbbbbbb");
+		
+		Iterator it = ts.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+	}
+}
