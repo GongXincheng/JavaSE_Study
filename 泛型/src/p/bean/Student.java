019dd194ca5ad424c998a56a408ace330579a740
GongXincheng <https://github.com/GongXincheng>
2017-07-23 13:00:30 +0800

    Student

---
+++ 泛型/src/p/bean/Student.java
@@ -0,0 +1,17 @@
+package p.bean;
+
+public class Student extends Person {
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
