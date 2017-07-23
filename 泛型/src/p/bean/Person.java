2196640a23c0883205ddbf939a3251c6689ea56b
GongXincheng <https://github.com/GongXincheng>
2017-07-23 12:21:28 +0800

    Person
    实现了  Comparable接口，重写了compareTo()方法，在TreeSet集合中按照年龄排序

---
+++ 泛型/src/p/bean/Person.java
@@ -0,0 +1,40 @@
+package p.bean;
+
+public class Person implements Comparable<Person> {
+	private String name;
+	private int age;
+	
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int compareTo(Person o) {		//按照年龄排序
+		// TODO Auto-generated method stub
+		int temp = this.age - o.age;
+		
+		return temp==0 ? this.name.compareTo(o.name) : temp;
+	}
+	
+	
+}
