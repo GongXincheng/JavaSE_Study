6536c50f9dd17268bce9d7b14580d875f5f970e0
GongXincheng <https://github.com/GongXincheng>
2017-07-23 13:22:10 +0800

    自定义泛型类
    将泛型定义在方法上
    当方法静态时，能不能访问类上定义的泛型, 如果静态方法使用泛型，只能将泛型定义在方法上！

--- 泛型/src/p2/generic/define/demo/Tool.java
+++ 泛型/src/p2/generic/define/demo/Tool.java
@@ -16,6 +16,29 @@ public class Tool<QQ> {
 	public void setObject(QQ object) {
 		this.q = object;
 	}
+	
+	/**
+	 * 将泛型定义在方法上
+	 * @param str
+	 */
+	public <W> void show(W str){
+		System.out.println("show : " + str);
+	}
+	
+	public void print(QQ str){
+		System.out.println("print : " + str);
+	}
+	
+	
+	/**
+	 * 当方法静态时，能不能访问类上定义的泛型
+	 * 如果静态方法使用泛型，只能将泛型定义在方法上！
+	 * @param obj
+	 */
+	public static <Y> void method( Y obj ){
+		System.out.println("method : " + obj);
+	}
+	
 }
 
 
