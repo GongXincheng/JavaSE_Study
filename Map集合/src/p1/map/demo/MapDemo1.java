9d74745acef0f39b9caf46a7611824eee0644dc9
GongXincheng <https://github.com/GongXincheng>
2017-07-24 11:03:51 +0800

    MapDemo1
    method2:使用map.keySet() 方法，返回Set集合 ，把map的所有键封装到set集合中；
    
    method3：使用map.entrySet() 方法，返回Set<Map.Entry<K,V>>集合 ，把map的键和值的映射关系封装到set集合中；映射关系的类型为：<Map.Entry<K,V>
    
    getMapValues：map.values()方法，获取map集合中的所有值

--- Map集合/src/p1/map/demo/MapDemo1.java
+++ Map集合/src/p1/map/demo/MapDemo1.java
@@ -1,5 +1,6 @@
 package p1.map.demo;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -9,30 +10,74 @@ public class MapDemo1 {
 	
 	public static void main(String[] args) {
 		Map<Integer,String> map = new HashMap<Integer,String>();
-//		method1(map);
-		method2(map);
-	}
-	
-	//重点方法：keySet();
-	public static void method2(Map<Integer,String> map){
-		
 		map.put(8, "wangwu");
 		map.put(2, "zhaoliu");
 		map.put(7, "xiaoqiang");
 		map.put(6, "wangcai");
 		
+//		method1(map);
+//		method2(map);
+		method3(map);
+		getMapValues(map);
+	}
+	
+	
+//获取map集合中的所有 值;
+	private static void getMapValues(Map<Integer, String> map) {
+			// TODO Auto-generated method stub
+		Collection<String> coll = map.values();
+		Iterator<String> it = coll.iterator();
+		while(it.hasNext()){
+			String value = it.next();
+			System.out.println(value);
+		}
+	}
+
+
+
+	//取出map中的所有元素
+	private static void method3(Map<Integer, String> map) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 取出map中的所有元素
+		 * 通过Map转成Set就可以迭代
+		 * 找到另一个方法：entrySet();
+		 *		 该方法将键和值的映射关系作为对象存储到了Set集合中，
+		 * 		而这个映射关系的类型就是Map.Entry类型(结婚证)
+		 */
+		
+		Set<Map.Entry<Integer, String>> entrySet = map.entrySet();
+		
+		Iterator<Map.Entry<Integer, String>> it = entrySet.iterator();
+		while(it.hasNext()){
+			Map.Entry<Integer, String> me = it.next();
+			Integer key = me.getKey();
+			String value = me.getValue();
+			System.out.println(key+" : "+value);
+		}
+		
+	}
+
+	
+	
+//重点方法：keySet();取出map中的所有元素
+	public static void method2(Map<Integer,String> map){
+		
 /*		取出map中的所有元素
 		 原理：通过Keyset方法获取map中所有键所在的Set集合
 				在通过Set的迭代器获取每一个键,再对每一个键通过map集合的get方法获取对应的值即可
 */
-		Set<Integer> keyset = map.keySet();
-		Iterator<Integer> it = keyset.iterator();
+		Set<Integer> keySet = map.keySet();
+		Iterator<Integer> it = keySet.iterator();
 		while(it.hasNext()){
 			Integer key = it.next();
 			String value = map.get(key);
 			System.out.println(key+" = "+value);
 		}
 	}
+	
+	
 	
 	public static void method1(Map<Integer,String> map){	//学号和姓名
 		
