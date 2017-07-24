eec2be394fed37db9948d71490f3ecb86c3f9380
GongXincheng <https://github.com/GongXincheng>
2017-07-24 16:57:34 +0800

    ComparatorByName
    TreeMap()的比较器，按名称排序

---
+++ Map集合/src/p3/treemap/demo/ComparatorByName.java
@@ -0,0 +1,17 @@
+package p3.treemap.demo;
+
+import java.util.Comparator;
+import p.bean.Person;
+
+public class ComparatorByName implements Comparator<Person> {
+
+	@Override
+	public int compare(Person o1, Person o2) {
+		// TODO Auto-generated method stub
+		
+		int temp = o1.getName().compareTo(o2.getName());
+		
+		return temp==0 ? o1.getAge()-o2.getAge() : temp;
+	}
+	
+}
