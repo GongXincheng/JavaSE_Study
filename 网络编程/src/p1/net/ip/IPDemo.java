1795d013234a231067bebc77ee328a2a01cc3f51
GongXincheng <757853223@qq.com>
2017-07-28 18:14:52 +0800

    IP

--- 网络编程/src/p1/net/ip/IPDemo.java
+++ 网络编程/src/p1/net/ip/IPDemo.java
@@ -13,10 +13,20 @@ public class IPDemo {
 		InetAddress ip = InetAddress.getLocalHost();//获取本地主机的 IP 地址
 		
 		//获取其他主机的ip地址对象
-		ip = InetAddress.getByName("192.168.1.100");	//afa
+		ip = InetAddress.getByName("www.baidu.com");
 		
 		System.out.println(ip.getHostAddress());	// 返回该IP 的 IP 地址字符串
 		System.out.println(ip.getHostName());		// 获取此 IP 地址的主机名。
 	}
 	
+	
+/*
+ 		192.168.1.101
+		SKY-20160824VZA
+		
+		192.168.1.102
+		WQ-20170502UVEM
+*/	
+	
+
 }
