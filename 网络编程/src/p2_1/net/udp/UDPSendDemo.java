30e3e4fe4b4c4d19ea646cfe8516b1254f2dde32
GongXincheng <757853223@qq.com>
2017-08-30 21:24:00 +0800

    UDP发送端
    * 创建UDP传输的发送端：
    	 * 思路：
    	 * 1. 建立UDP的socket服务(端点)
    	 * 2. 将要发送的数据封装到 数据包(DatagramPacket) 中
    	 * 3. 通过UDP的socket服务将数据包发送出去
    	 * 4. 关闭socket服务。

---
+++ 网络编程/src/p2_1/net/udp/UDPSendDemo.java
@@ -0,0 +1,35 @@
+package p2_1.net.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPSendDemo {
+public static void main(String[] args) throws IOException {
+	System.out.println("发送端启动...");
+	/*
+	 * 创建UDP传输的发送端：
+	 * 思路：
+	 * 1. 建立UDP的socket服务(端点)
+	 * 2. 将要发送的数据封装到 数据包(DatagramPacket) 中
+	 * 3. 通过UDP的socket服务将数据包发送出去
+	 * 4. 关闭socket服务。
+	 */
+	
+	//1.建立UDP的socket服务。使用DatagramSocket对象.
+	DatagramSocket ds = new DatagramSocket();
+	
+	//2.将要发送的数据封装到数据包中
+	String str = "UDP传输演示....";
+	//使用DatagramPacket 将数据封装到该对象包中
+	byte[] buf = str.getBytes();
+	DatagramPacket dp =  new DatagramPacket(buf, buf.length, InetAddress.getByName("localhost"),10000);
+	
+	//3.通过UDP的socket服务将数据包发送出去
+	ds.send(dp);
+	
+	//4.关闭资源
+	ds.close();
+}
+}
